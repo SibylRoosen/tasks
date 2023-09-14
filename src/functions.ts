14575c224ed1975fe596d0f7e9210452fd1d6470
Sibyl Roosen <sibyl@udel.edu>
2023-09-13 21:04:25 -0400

    Eddited shout and isQuestion to do the task that it needed to do, and pass the required tests.

--- src/functions.ts
+++ src/functions.ts
@@ -32,7 +32,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    const up = message.toUpperCase() + "!";
+    return up;
 }
 
 /**
@@ -40,7 +41,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
