c8207a6e4cebfff361f67b8241d24bf03c0eb244
Sibyl Roosen <sibyl@udel.edu>
2023-09-27 19:38:37 -0400

    made a mistake

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Puck from "./Puck.jpeg";
 import "./App.css";
+<<<<<<< HEAD
 import { Button, Col, Container, Row } from "react-bootstrap";
+=======
+import { ChangeType } from "./components/ChangeType";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
+import { Counter } from "./components/Counter";
+>>>>>>> eb40f3eb8827e668c1949ca0c9c13db2f52fe4b4
 
 function App(): JSX.Element {
     return (
@@ -9,6 +18,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+<<<<<<< HEAD
             <h1>This is Sibyl Roosens Heading</h1>
             <Container>
                 <Row>
@@ -40,6 +50,20 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+=======
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
+>>>>>>> eb40f3eb8827e668c1949ca0c9c13db2f52fe4b4
         </div>
     );
 }
