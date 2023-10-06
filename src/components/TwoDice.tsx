d08df0dfde765c1320d7dffd56e414518ca2f36b
Sibyl Roosen <sibyl@udel.edu>
2023-10-05 22:46:52 -0400

    Cycle Holiday was added and works, but two dice still fails 5 tests

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -13,35 +13,41 @@ export function d6(): number {
 
 export function TwoDice(): JSX.Element {
     const [dieLeft, changeDieLeft] = useState<number>(d6());
-    const [dieRight, changeDieRight] = useState<number>(() => {
+    const generateDieRight = (): number => {
         let initialDieRight = d6();
         while (initialDieRight === dieLeft) {
             initialDieRight = d6();
         }
         return initialDieRight;
-    });
+    };
+    const [dieRight, changeDieRight] = useState<number>(generateDieRight);
     function rollLeft(): void {
         changeDieLeft(d6());
     }
     function rollRight(): void {
         changeDieRight(d6());
     }
-    const resultMessage = () => {
-        if (dieLeft === 1 && dieRight === 1) {
-            return "Lose";
-        } else if (dieLeft === dieRight) {
-            return "Win";
-        } else {
-            return "Keep rolling... Try Again!";
-        }
-    };
     return (
         <div>
-            <span data-testid="right-die">{dieRight}</span>
-            <span data-testid="left-die">{dieLeft}</span>
-            <Button onClick={rollRight}>Roll Right</Button>
-            <Button onClick={rollLeft}>Roll Left</Button>
-            <span>{resultMessage}</span>
+            <div>
+                <span data-testid="left-die">{dieLeft}</span>
+                <span data-testid="right-die">{dieRight}</span>
+            </div>
+            <div>
+                <span>
+                    <Button onClick={rollLeft}>Roll Left</Button>
+                    <Button onClick={rollRight}>Roll Right</Button>
+                </span>
+            </div>
+            <div>
+                {dieLeft === 1 && dieRight === 1 ? (
+                    <span>You Lose</span>
+                ) : dieLeft === dieRight ? (
+                    <span>You Win</span>
+                ) : (
+                    <span>Try Again</span>
+                )}
+            </div>
         </div>
     );
 }
