62bc4a1252bc24ef16855538a0bfc1023e5b4d8a
Sibyl Roosen <sibyl@udel.edu>
2023-10-13 23:38:17 -0400

    Updated everything to pass tests, change color fails a test, but it does not work properly if it passes the test, multiple choice question passes everything though.

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,68 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const colors = [
+        "red",
+        "yellow",
+        "orange",
+        "green",
+        "teal",
+        "blue",
+        "indigo",
+        "magenta"
+        // Add more colors if needed
+    ];
+
+    const [chosenColor, setChosenColor] = useState<string | null>(null);
+
+    const handleColorChange = (color: string) => {
+        setChosenColor(color);
+    };
+
     return (
         <div>
             <h3>Change Color</h3>
+            <Form>
+                {colors.map((color) => (
+                    <Form.Check
+                        key={color}
+                        type="radio"
+                        inline
+                        label={
+                            <>
+                                <div
+                                    style={{
+                                        width: "100px",
+                                        height: "20px",
+                                        backgroundColor: color,
+                                        marginTop: "10px",
+                                        display: "inline-block",
+                                        padding: "10px"
+                                    }}
+                                />
+                                {color}
+                            </>
+                        }
+                        id={`color-${color}`}
+                        checked={chosenColor === color}
+                        onChange={() => handleColorChange(color)} //{(e) => setChosenColor(e.target.value)} this method passed the test but did not work on the websight
+                    />
+                ))}
+            </Form>
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "250px",
+                    height: "50px",
+                    backgroundColor: chosenColor || "white",
+                    marginTop: "10px",
+                    display: "inline-block",
+                    padding: "10px"
+                }}
+            >
+                {"You have chosen " + chosenColor + "." || "No color selected"}
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [currChoice, setCurrChoice] = useState<string>(options[0]);
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurrChoice(event.target.value);
+    }
+    const isCorrect = currChoice === expectedAnswer;
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="correctChoice">
+                <Form.Label>Select your Choice</Form.Label>
+                <Form.Select value={currChoice} onChange={updateChoice}>
+                    {options.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{isCorrect ? "✔️" : "❌"}</div>
         </div>
     );
 }
